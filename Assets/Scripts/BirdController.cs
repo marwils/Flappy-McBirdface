0d5bc48249cb146d4761495271eb296410743566
Martin Wilsdorf <de.marwils@gmail.com>
2021-02-18 20:59:36 +0100

    Implement bird movement

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BirdMovement))]
+public class BirdController : MonoBehaviour
+{
+    public float m_UpForce;
+
+    private BirdMovement m_BirdMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_BirdMovement = GetComponent<BirdMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_BirdMovement.Flap(m_UpForce);
+        }
+
+    }
+}
---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdMovement : MonoBehaviour
+{
+    public float m_TiltSpeed;
+
+    private Quaternion m_UpRotation = Quaternion.Euler(0, 0, 35);
+    private Quaternion m_DownRotation = Quaternion.Euler(0, 0, -70);
+
+    private Rigidbody2D m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, m_DownRotation, m_TiltSpeed * Time.deltaTime);
+    }
+
+    public void Flap(float force)
+    {
+        transform.rotation = m_UpRotation;
+        m_Rigidbody.velocity = Vector2.zero;
+        m_Rigidbody.AddForce(Vector2.up * force, ForceMode2D.Force);
+    }
+}
