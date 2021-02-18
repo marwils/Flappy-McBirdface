491d797b128bbbd50a2697601fd479036e936565
Martin Wilsdorf <de.marwils@gmail.com>
2021-02-18 20:35:26 +0100

    Add Y-Inverter for keeping pipes distance

---
+++ Assets/Scripts/YInverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YInverter : MonoBehaviour
+{
+    public Transform m_LowerObject;
+    public Transform m_UpperObject;
+
+    public float m_Distance;
+
+    void Update()
+    {
+        m_LowerObject.localPosition = Vector2.up * -m_Distance;
+        m_UpperObject.localPosition = Vector2.up * m_Distance;
+    }
+}
