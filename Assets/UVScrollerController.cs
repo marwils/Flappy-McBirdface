4fd18d3ff4c06ccc6e0f84b340cc5ff3a337ee4f
Martin Wilsdorf <de.marwils@gmail.com>
2021-02-19 23:32:10 +0100

    Add UV Scroller Controller

---
+++ Assets/UVScrollerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UVScroller))]
+public class UVScrollerController : MonoBehaviour
+{
+    private UVScroller m_UVScroller;
+
+    private void Start()
+    {
+        m_UVScroller = GetComponent<UVScroller>();
+    }
+
+    private void OnEnable()
+    {
+        BirdController.BirdHit += OnBirdHit;
+        GameManager.GameStarted += OnGameStarted;
+    }
+
+    private void OnDisable()
+    {
+        BirdController.BirdHit -= OnBirdHit;
+        GameManager.GameStarted -= OnGameStarted;
+    }
+
+    private void OnBirdHit()
+    {
+        m_UVScroller.enabled = false;
+    }
+
+    private void OnGameStarted()
+    {
+        m_UVScroller.enabled = true;
+    }
+}
